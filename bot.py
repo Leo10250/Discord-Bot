be214789f1798541d703501fed241caccf9b36a5
Leo10250 <57935115+Leo10250@users.noreply.github.com>
2020-09-17 13:15:23 -0700

    bruh

--- bot.py
+++ bot.py
@@ -4,8 +4,6 @@ from discord.ext import commands
 
 client = commands.Bot(command_prefix = '!')
 
-token = open('file').read()
-
 token = json.load(open('secrets.json', 'r'))['secret']
 
 @client.event
